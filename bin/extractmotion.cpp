4f63ceef76304ebc52722ad08af46ae14ceb7f45
seppul@utu.fi <seppul@utu.fi@1d115e8e-bafe-11de-b431-8336cfaced44>
2010-01-13 10:02:12 +0000

    Command-line interface rewritten with boost::program_options.

--- bin/extractmotion.cpp
+++ bin/extractmotion.cpp
@@ -12,15 +12,18 @@
 #include "PyramidalLucasKanade.h"
 #include "PyramidalProesmans.h"
 #include "SparseImageExtrapolator.h"
+#include "version.h"
 
-#include <argtable2.h>
+//#include <argtable2.h>
+#include <boost/program_options.hpp>
 #include <cstdlib>
 #include <cstring>
 #include <string>
 
+using namespace boost::program_options;
 using namespace std;
 
-struct ArgData
+/*struct ArgData
 {
   string srcImgFileName1;
   string srcImgFileName2;
@@ -289,7 +292,7 @@ static bool parseArgs(int argc, char **argv, ArgData &result)
     printf("invalid extrapolation method\n");
     success = false;
   }*/
-  if(success == false)
+  /*if(success == false)
   {
     delete result.denseMotionExtractor;
 #ifdef WITH_CGAL
@@ -304,7 +307,7 @@ static bool parseArgs(int argc, char **argv, ArgData &result)
   return success;
 }
 
-int main(int argc, char **argv)
+/*int main(int argc, char **argv)
 {
   ArgData argData;
   
@@ -323,6 +326,154 @@ int main(int argc, char **argv)
                                                     argData.srcImgFileName2,
                                                     argData.outFileNamePrefix);
 #endif
+  
+  return EXIT_SUCCESS;
+}*/
+
+int main(int argc, char **argv)
+{
+  DenseMotionExtractor *denseMotionExtractor = NULL;
+#ifdef WITH_CGAL
+  SparseMotionExtractor *sparseMotionExtractor = NULL;
+#endif
+  
+  options_description generalArgs("General options");
+  generalArgs.add_options()
+    ("help", "print usage")
+    ("options", value< std::string >(), "print options specific to the algorithm <arg>")
+    ("version", "print version number");
+  
+  options_description reqArgs("Required arguments");
+  reqArgs.add_options()
+    ("image1", value< std::string >(), "first image")
+    ("image2", value< std::string >(), "second image")
+    ("algorithm", value< std::string >(), "motion detection algorithm (lucaskanade, opencv, proesmans)")
+    ("outprefix", value< std::string >(), "output file prefix");
+  
+  // options specific to the Lucas-Kanade algorithm
+  options_description lucasKanadeArgs("Options for the Lucas-Kanade algorithm");
+  lucasKanadeArgs.add_options()
+    ("numlevels",    value< int >(),   "number of pyramid levels (default = ?)")
+    ("windowradius", value< int >(),   "feature matching window radius (default = ?)")
+    ("numiter",      value< int >(),   "number of iterations (default = ?)")
+    ("tau",          value< float >(), "eigenvalue threshold for feature matching (default = ?)")
+    ("sigmap",       value< float >(), "regularization parameter (default = ?)");
+  
+  // options specific to the Lucas-Kanade algorithm (OpenCV)
+  options_description opencvArgs("Options for the Lucas-Kanade algorithm (OpenCV)");
+  opencvArgs.add_options()
+    ("numlevels",    value< int >(), "number of pyramid levels (default = ?)")
+    ("windowsize",   value< int >(), "feature matching window size (default = ?)")
+    ("maxnumpoints", value< int >(), "maximum number of feature points (default = ?)")
+    ("minpointdist", value< int >(), "minimum distance between feature points (default = ?)")
+    ("qualitylevel", value< int >(), "feature point quality threshold (default = ?)")
+    ("maxnumiter",   value< int >(), "maximum number of iterations (default = ?)")
+    ("epsilon",      value< int >(), "stopping criterion threshold (default = ?)");
+  
+  // options specific to the Proesmans algorithm
+  options_description proesmansArgs("Options for the Proesmans algorithm");
+  proesmansArgs.add_options()
+    ("numlevels", value< int >(),   "number of pyramid levels (default = ?)")
+    ("numiter",   value< int >(),   "number of iterations (default = ?)")
+    ("lambda",    value< float >(), "smoothness parameter (default = ?)")
+    ("boundcond", value< int >(),   "boundary conditions (0 = Dirichlet, 1 = Neumann)  (default = ?)");
+  
+  options_description allArgs("");
+  allArgs.add(generalArgs).add(reqArgs).add(lucasKanadeArgs).add(proesmansArgs).add(opencvArgs);
+  options_description allVisibleArgs("Usage: extractmotion <required arguments> [algorithm-specific options]");
+  allVisibleArgs.add(generalArgs).add(reqArgs);
+  
+  try {
+    variables_map vm;
+    store(parse_command_line(argc, argv, allArgs), vm);
+    notify(vm);
+    
+    if(vm.size() == 0 || vm.count("help"))
+    {
+      std::cout<<allVisibleArgs<<std::endl;
+      return EXIT_SUCCESS;
+    }
+    else if(vm.count("version"))
+      std::cout<<OPTFLOW_VERSION_INFO<<std::endl;
+    else if(vm.count("options") && vm["options"].as< string >() == "lucaskanade")
+      std::cout<<lucasKanadeArgs<<std::endl;
+    else if(vm.count("options") && vm["options"].as< string >() == "opencv")
+      std::cout<<opencvArgs<<std::endl;
+    else if(vm.count("options") && vm["options"].as< string >() == "proesmans")
+      std::cout<<proesmansArgs<<std::endl;
+    else if(!vm.count("image1") || !vm.count("image2") || !vm.count("algorithm"))
+    {
+      std::cout<<"One or more required arguments missing."<<std::endl;
+      std::cout<<reqArgs<<std::endl;
+    }
+    
+    if(vm["algorithm"].as< string >() == "lucaskanade")
+    {
+      denseMotionExtractor = new PyramidalLucasKanade(
+        vm.count("windowradius") > 0 ? vm["windowradius"].as< int >() : 16,
+        vm.count("numiter") > 0      ? vm["numiter"].as< int >() : 5,
+        vm.count("tau") > 0          ? vm["tau"].as< float >() : 0.0025,
+        vm.count("sigmap") > 0       ? vm["sigmap"].as< float >() : 0.0,
+        vm.count("numlevels") > 0    ? vm["numlevels"].as< int >() : 4,
+        true);
+    }
+    else if(vm["algorithm"].as< string >() == "opencv")
+    {
+      sparseMotionExtractor = new LucasKanadeOpenCV(
+        vm.count("numlevels") > 0    ? vm["numlevels"].as< int >() : 4,
+        vm.count("windowsize") > 0   ? vm["windowsize"].as< int >() : 40,
+        vm.count("maxnumpoints") > 0 ? vm["maxnumpoints"].as< int >() : 1000,
+        vm.count("minpointdist") > 0 ? vm["minpointdist"].as< float >() : 10,
+        vm.count("qualitylevel") > 0 ? vm["qualitylevel"].as< float >() : 0.025,
+        vm.count("maxnumiter") > 0   ? vm["maxnumiter"].as< int >() : 10,
+        vm.count("epsilon") > 0      ? vm["epsilon"].as< float >() : 0.001);
+    }
+    else if(vm["algorithm"].as< string >() == "proesmans")
+    {
+      Proesmans::BoundaryConditions boundCond;
+      
+      int bc = 1;
+      if(vm.count("boundcond") > 0)
+        bc = vm["boundcond"].as< int >();
+      if(bc == 0)
+        boundCond = Proesmans::DIRICHLET;
+      else
+        boundCond = Proesmans::NEUMANN;
+      
+      denseMotionExtractor = new PyramidalProesmans(
+        vm.count("numiter") > 0   ? vm["numiter"].as< int >() : 200,
+        vm.count("lamda") > 0     ? vm["lambda"].as< float >() : 100.0,
+        vm.count("numlevels") > 0 ? vm["numlevels"].as< int >() : 4,
+        boundCond);
+    }
+    else
+    {
+      std::cout<<"Invalid algorithm name."<<std::endl;
+      return EXIT_SUCCESS;
+    }
+  
+    std::string srcImgFileName1 = vm["image1"].as< string >();
+    std::string srcImgFileName2 = vm["image2"].as< string >();
+    std::string outFilePrefix   = vm["outprefix"].as< string >();
+    
+    if(denseMotionExtractor != NULL)
+    {
+      MotionExtractorDriver::runDenseMotionExtractor(
+        *denseMotionExtractor, srcImgFileName1, srcImgFileName2, outFilePrefix);
+      delete denseMotionExtractor;
+    }
+#ifdef WITH_CGAL
+    else if(sparseMotionExtractor != NULL)
+    {
+      MotionExtractorDriver::runSparseMotionExtractor(
+        *sparseMotionExtractor, srcImgFileName1, srcImgFileName2, outFilePrefix);
+      delete sparseMotionExtractor;
+    }
+#endif
+  }
+  catch(std::exception &e) {
+    std::cout<<allVisibleArgs<<std::endl;
+  }
   
   return EXIT_SUCCESS;
 }
--- bin/extrapolate.cpp
+++ bin/extrapolate.cpp
@@ -9,15 +9,18 @@
 #include "InverseDenseImageExtrapolator.h"
 #include "SparseImageExtrapolator.h"
 #include "SparseVectorFieldIO.h"
+#include "version.h"
 
-#include <argtable2.h>
+//#include <argtable2.h>
+#include <boost/program_options.hpp>
 #include "CImg_config.h"
 #include <CImg.h>
 #include <cstdlib>
 
+using namespace boost::program_options;
 using namespace cimg_library;
 
-struct ArgData
+/*struct ArgData
 {
   CImg< unsigned char > I0;
   CImg< double > *Vd;
@@ -165,6 +168,99 @@ int main(int argc, char **argv)
                                                        *argData.Vd,
                                                        argData.numSteps,
                                                        argData.outFileNamePrefix);
+  
+  return EXIT_SUCCESS;
+}*/
+
+int main(int argc, char **argv)
+{
+  InverseDenseImageExtrapolator *denseExtrapolator = NULL;
+  CImg< double > *Vd = NULL;
+#if defined (WITH_OPENCV) && defined(WITH_CGAL)
+  SparseImageExtrapolator *sparseExtrapolator = NULL;
+  SparseVectorField *Vs = NULL;
+#endif
+  CImg< unsigned char > I0;
+  
+  options_description generalArgs("General options");
+  generalArgs.add_options()
+    ("help", "print usage")
+    ("version", "print version number");
+  
+  options_description reqArgs("Required arguments");
+  reqArgs.add_options()
+    ("image", value< std::string >(), "the image to extrapolate")
+    ("motionfield", value< std::string >(), "the motion field to use")
+    ("numtimesteps", value< int >(), "number of images to extrapolate")
+    ("outprefix", value< std::string >(), "output file prefix");
+  
+  options_description allArgs("Usage: extractmotion <required arguments> [algorithm-specific options]");
+  allArgs.add(generalArgs).add(reqArgs);
+  
+  try {
+    variables_map vm;
+    store(parse_command_line(argc, argv, allArgs), vm);
+    notify(vm);
+    
+    if(vm.size() == 0 || vm.count("help"))
+    {
+      std::cout<<allArgs<<std::endl;
+      return EXIT_SUCCESS;
+    }
+    else if(vm.count("version"))
+      std::cout<<OPTFLOW_VERSION_INFO<<std::endl;
+    else if(!vm.count("image") || !vm.count("motionfield") || 
+            !vm.count("numtimesteps") || !vm.count("outprefix"))
+    {
+      std::cout<<"One or more required arguments missing."<<std::endl;
+      std::cout<<reqArgs<<std::endl;
+    }
+    
+    std::string imageFileName       = vm["image"].as< std::string >();
+    std::string motionFieldFileName = vm["motionfield"].as< std::string >();
+    int numTimeSteps                = vm["numtimesteps"].as< int >();
+    std::string outPrefix           = vm["outprefix"].as< std::string >();
+    
+    I0              = CImg< unsigned char >(imageFileName.c_str());
+    std::string ext = motionFieldFileName.substr(motionFieldFileName.size() - 4, 4);
+    if(ext == "pdvm")
+    {
+      Vd = new CImg< double >();
+      DenseVectorFieldIO::readVectorField(motionFieldFileName, *Vd);
+      denseExtrapolator = new InverseDenseImageExtrapolator();
+    }
+#if defined (WITH_OPENCV) && defined(WITH_CGAL)
+    else if(ext == "psvm")
+    {
+      Vs = new SparseVectorField();
+      SparseVectorFieldIO::readVectorField(motionFieldFileName, *Vs);
+      Vs->triangulate();
+      sparseExtrapolator = new SparseImageExtrapolator();
+    }
+#endif
+    else
+    {
+      std::cout<<"Unknown motion field file extension."<<std::endl;
+      return EXIT_SUCCESS;
+    }
+    
+#ifdef WITH_CGAL
+    if(sparseExtrapolator != NULL)
+      ImageExtrapolatorDriver::runSparseImageExtrapolator(
+        *sparseExtrapolator, I0, *Vs, numTimeSteps, outPrefix);
+    else
+#endif
+      ImageExtrapolatorDriver::runDenseImageExtrapolator(
+        *denseExtrapolator, I0, *Vd, numTimeSteps, outPrefix);
+  }
+  catch(std::exception &e) {
+    std::cout<<allArgs<<std::endl;
+  }
+  
+  delete denseExtrapolator;
+  delete sparseExtrapolator;
+  delete Vd;
+  delete Vs;
   
   return EXIT_SUCCESS;
 }
